4a6dc8057d02013884a075794ef355a7ae7b0a92
Alan C <alanc091322@gmail.com>
2024-05-08 15:12:32 -0700

    initialized Express and Sequelize with CSRF protection without migration files

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get('/hello/world', function(req, res) {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
+// router.get('/hello/world', function(req, res) {
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
+
+// add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    'XSRF-Token': csrfToken
+  });
 });
+// This route should not be available in production, but it will not be exclusive to the production application until you implement the frontend of the application later. So for now, it will remain available to both the development and production environments.
 
 
 module.exports = router;
