7b26f1b9a78a80331fe45110932e5d97003f925c
Alan C <alanc091322@gmail.com>
2024-05-09 15:53:50 -0700

    phase 3 added Users table migration and seeds

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,22 +1,40 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model, Validator } = require('sequelize');
+
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-
     static associate(models) {
-      User.hasMany(models.Spot, {foreignKey: "ownerId"});
-      User.hasMany(models.Booking, {foreignKey: "userId"});
-      User.hasMany(models.Review, {foreignKey: "userId"});
+      // define association here
     }
   }
   User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    username: DataTypes.STRING,
-    hashedPassword: DataTypes.STRING,
-    email: DataTypes.STRING
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [4, 30],
+        isNotEmail(value) {
+          if (Validator.isEmail(value)) throw Error("Cannot be an email.");
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [3, 256],
+        isEmail: true
+      }
+    },
+    hashedPassword: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [60, 60]
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',
