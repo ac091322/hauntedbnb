7f7b02e466eaa29b5955e827f4b644416b02c516
Alan C <alanc091322@gmail.com>
2024-05-13 13:25:04 -0700

    phase 4 added User login backend endpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,16 +1,20 @@
 const router = require("express").Router();
-
-// const { setTokenCookie } = require('../../utils/austh.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
+// const { setTokenCookie } = require('../../utils/austh.js');
 // const { requireAuth } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 
 
 router.use(restoreUser);
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
 
-// router.post("/test", function (req, res) {
-//   res.json({ requestBody: req.body });
-// });
+
+router.post("/test", function (req, res) {
+  res.json({ requestBody: req.body });
+});
 
 // GET /api/set-token-cookie
 // router.get('/set-token-cookie', async (_req, res) => {
