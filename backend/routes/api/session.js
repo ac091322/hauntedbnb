ece49a911959abe7dd88ae298f29644468c5c515
Alan C <alanc091322@gmail.com>
2024-05-14 16:24:45 -0700

    adjusted order of user keys

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -43,8 +43,8 @@ router.post('/', validateLogin, async (req, res, next) => {
 
   const safeUser = {
     id: user.id,
-    email: user.email,
     username: user.username,
+    email: user.email,
     firstName: user.firstName,
     lastName: user.lastName
   };
@@ -70,8 +70,8 @@ router.get('/', (req, res) => {
   if (user) {
     const safeUser = {
       id: user.id,
-      email: user.email,
       username: user.username,
+      email: user.email,
       firstName: user.firstName,
       lastName: user.lastName
     };
