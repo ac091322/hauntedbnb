22ceaf02a92d4be6760e557ab0578701ad5d1a5b
Alan C <alanc091322@gmail.com>
2024-05-31 22:33:27 -0700

    modified vite.config to automatically open

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,8 +15,7 @@ export default defineConfig(({ mode }) => ({
     proxy: {
       '/api': 'http://localhost:8000'
     },
-    // To automatically open the app in the browser whenever the server starts,
-    // uncomment the following line:
-    // open: true
+    // to automatically open the app in the browser whenever the server starts, uncomment the following line:
+    open: true
   }
 }));
