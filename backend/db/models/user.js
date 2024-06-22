b54c48cfdac22f27b98e0db6958cf750add04e4b
alan-chang <alanc091322@gmail.com>
2024-06-21 20:39:30 -0700

    added .BINARY back, it didn't make a difference

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -55,7 +55,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false
     },
     hashedPassword: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING.BINARY,
       allowNull: false,
       validate: {
         len: [60, 60]
